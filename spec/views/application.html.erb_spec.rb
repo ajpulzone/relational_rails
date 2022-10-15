336c5a677c9c1ecf1ae280b1f8dc35af0bbf7e63
Annie Pulzone <ajpulzone@gmail.com>
2022-10-15 15:20:46 -0700

    Add link to grow_zones index to every page and functionality testing

--- spec/views/application.html.erb_spec.rb
+++ spec/views/application.html.erb_spec.rb
@@ -2,12 +2,28 @@ require "rails_helper"
 
 describe "layouts/application", type: :view do
   
-  it "should display a link to '/plants/' on every page" do
-    render
-    rendered.should have_selector(:css, 'a[href="/plants/"]')
-    
-    expect(rendered).to have_selector(:css, 'a[href="/plants/"]')
-
-    # expect(page).to have_link(href="/plants/")
+  #User story 8
+  describe "As a visitor" do
+    describe "When I visit any page on the site" do
+      it "should display a link to '/plants/' on every page" do
+        render
+        rendered.should have_selector(:css, 'a[href="/plants/"]')
+        
+        expect(rendered).to have_selector(:css, 'a[href="/plants/"]')
+      end
+    end 
   end
+
+  #User story 9
+  describe "As a visitor" do
+    describe "When I visit any page on the site" do
+      it "should display a link to '/grow_zones/' on every page" do
+        render
+        rendered.should have_selector(:css, 'a[href="/grow_zones/"]')
+
+        expect(rendered).to have_selector(:css, 'a[href="/grow_zones/"]')
+      end
+    end 
+  end 
+
 end
